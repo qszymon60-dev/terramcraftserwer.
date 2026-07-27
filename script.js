ed4c35b022a12e9f97a41e895a42bced00101f6a
qszymon60-dev <qszymon60@gmail.com>
2026-07-27 21:03:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ function copyIP() {
         alert("❌ Nie udało się skopiować IP.");
     });
 }
+
+async function checkServer() {
+    const status = document.getElementById("server-status");
+
+    try {
+        const response = await fetch(
+            "https://api.mcsrvstat.us/2/terramcraft.falix.gg"
+        );
+
+        const data = await response.json();
+
+        if (data.online) {
+            status.innerHTML =
+                "🟢 Serwer ONLINE<br>" +
+                "👥 Gracze: " +
+                data.players.online +
+                "/" +
+                data.players.max;
+        } else {
+            status.innerHTML =
+                "🔴 Serwer OFFLINE";
+        }
+
+    } catch {
+        status.innerHTML =
+            "⚠️ Nie można sprawdzić serwera";
+    }
+}
+
+checkServer();
